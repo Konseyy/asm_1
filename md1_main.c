a6733f97345abcdf52e6512e3ed1dde46a7942ec
Valdis Guntis Bukalders <vgbukalders@gmail.com>
2023-10-22 17:06:19 +0300

    changes

--- md1_main.c
+++ md1_main.c
@@ -6,12 +6,15 @@ int main(int argc, char *argv[])
 {
   if (argc != 2)
   {
+    // Wrong number of arguments
     return -1;
   }
-  
-  int arg = atoi(argv[1]);
-  unsigned int res = asum((unsigned int)arg);
-  printf("%u\n", res);
+
+  int n = atoi(argv[1]);
+
+  unsigned int sum = asum((unsigned int)n);
+
+  printf("%u\n", sum);
 
   return 0;
 }
